9b42eb74c7201a5f527bb0d86216917a401df79e
akiraabe <akiraabe2005@yahoo.co.jp>
2017-04-30 02:10:07 +0900

    Modify data.sql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,2 +1,2 @@
-insert into book (id, title, author, publisher, publish_date) values(1,'Domain Driven Design','Eric Evans', 'ORAILEY', '2017-04-29');
-insert into book (id, title, author, publisher, publish_date) values(2,'Pattern of Enterprise Application Architecture','Martin Fowler', 'ORAILEY', '2000-04-29');
+insert into book (id, title, author, publisher, publish_date) values(1,'Domain Driven Design','Eric Evans', 'Test', '1900-01-01');
+insert into book (id, title, author, publisher, publish_date) values(2,'Pattern of Enterprise Application Architecture','Martin Fowler', 'Test-Publisher', '2000-12-31');
