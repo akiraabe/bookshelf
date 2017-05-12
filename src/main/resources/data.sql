b6dd4fa1a09a1552f195fe2684d2af519fb4e57a
akiraabe <akiraabe2005@yahoo.co.jp>
2017-05-13 08:49:27 +0900

    Change database from H2 to postgresql.

--- src/main/resources/data.sql
+++
@@ -1,2 +0,0 @@
-insert into book (id, title, author, publisher, publish_date) values(1,'Domain Driven Design','Eric Evans', 'Test', '1900-01-01');
-insert into book (id, title, author, publisher, publish_date) values(2,'Pattern of Enterprise Application Architecture','Martin Fowler', 'Test-Publisher', '2000-12-31');
